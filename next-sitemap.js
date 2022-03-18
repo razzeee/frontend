584069717c57331dfeb25d96ba5e73b4ee8fcea5
Kolja Lampe <razzeee@gmail.com>
2022-03-18 02:52:03 +0100

    Update sitemap and declare languages

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,48 @@
+/** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_BASE_URI,
   generateRobotsTxt: true,
+  alternateRefs: [
+    {
+      hreflang: 'en',
+      href: process.env.NEXT_PUBLIC_SITE_BASE_URI + '/en',
+    },
+    {
+      hreflang: 'de',
+      href: process.env.NEXT_PUBLIC_SITE_BASE_URI + '/de',
+    },
+    {
+      hreflang: 'fr',
+      href: process.env.NEXT_PUBLIC_SITE_BASE_URI + '/fr',
+    },
+    {
+      hreflang: 'nb_NO',
+      href: process.env.NEXT_PUBLIC_SITE_BASE_URI + '/nb_NO',
+    },
+    {
+      hreflang: 'tr',
+      href: process.env.NEXT_PUBLIC_SITE_BASE_URI + '/tr',
+    },
+    {
+      hreflang: 'fi',
+      href: process.env.NEXT_PUBLIC_SITE_BASE_URI + '/fi',
+    },
+    {
+      hreflang: 'id',
+      href: process.env.NEXT_PUBLIC_SITE_BASE_URI + '/id',
+    },
+    {
+      hreflang: 'it',
+      href: process.env.NEXT_PUBLIC_SITE_BASE_URI + '/it',
+    },
+    {
+      hreflang: 'pl',
+      href: process.env.NEXT_PUBLIC_SITE_BASE_URI + '/pl',
+    },
+    {
+      hreflang: 'pt_BR',
+      href: process.env.NEXT_PUBLIC_SITE_BASE_URI + '/pt_BR',
+    },
+  ],
 }
--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 const { i18n } = require('./next-i18next.config')
 
 module.exports = {
